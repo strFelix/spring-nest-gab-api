f50317874967d68f72629d9cfcf759009134154f
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2026-05-18 20:27:50 -0300

    initial commit

---
+++ src/main/java/br/com/gabnest/nest_gab_api/NestGabApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabnest.nest_gab_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestGabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestGabApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabnest/nest_gab_api/NestGabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabnest.nest_gab_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestGabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
